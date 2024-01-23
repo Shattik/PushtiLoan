b61b21d743bc5b9c6e77086ac904432219b82493
Shattik <1905008@ugrad.cse.buet.ac.bd>
2024-01-23 17:21:18 +0600

    historyDoneWithOngoing

--- loanHistory.js
+++ loanHistory.js
@@ -8,9 +8,16 @@ router.route("/farmer").post(async (req, res) => {
     try {
         let size = await supabase.any(`SELECT count(*) FROM "FarmerLoan" WHERE "farmerId" = $1 AND "status" != $2`, [farmer_id, "ongoing"]);
         let data = await supabase.any(`SELECT "id", "approvedAmount", "remainingAmount", "status", "approvalTime", "finishTime" FROM "FarmerLoan" WHERE "farmerId" = $1 AND "status" != $2 order by "id" limit 50 offset $3`, [farmer_id, "ongoing" ,page*50]);
+        let ongoing = await supabase.any(`SELECT "id", "approvedAmount", "remainingAmount", "status", "approvalTime", "finishTime" FROM "FarmerLoan" WHERE "farmerId" = $1 AND "status" = $2`, [farmer_id, "ongoing"]);
         const response = {
             size: size[0].count,
             data: data
+        }
+        if(ongoing.length > 0) {
+            response.ongoing = ongoing[0];
+        }
+        else {
+            response.ongoing = null;
         }
         res.json(response);
     }
@@ -27,9 +34,16 @@ router.route("/sme").post(async (req, res) => {
     try {
         let size = await supabase.any(`SELECT count(*) FROM "SmeLoan" WHERE "smeId" = $1 AND "status" != $2`, [sme_id, "ongoing"]);
         let data = await supabase.any(`SELECT "id", "approvedAmount", "remainingAmount", "status", "approvalTime", "finishTime" FROM "SmeLoan" WHERE "smeId" = $1 AND "status" != $2 order by "id" limit 50 offset $3`, [sme_id, "ongoing" ,page*50]);
+        let ongoing = await supabase.any(`SELECT "id", "approvedAmount", "remainingAmount", "status", "approvalTime", "finishTime" FROM "SmeLoan" WHERE "smeId" = $1 AND "status" = $2`, [sme_id, "ongoing"]);
         const response = {
             size: size[0].count,
             data: data
+        };
+        if(ongoing.length > 0) {
+            response.ongoing = ongoing[0];
+        }
+        else {
+            response.ongoing = null;
         }
         res.json(response);
     }
