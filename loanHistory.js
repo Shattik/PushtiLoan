7922132776525372395ca27d5b26abd918e4d7cc
Shattik <1905008@ugrad.cse.buet.ac.bd>
2024-01-23 16:59:43 +0600

    historyDone

---
+++ loanHistory.js
@@ -0,0 +1,42 @@
+const supabase = require("./db.js");
+const router = require("express").Router();
+
+router.route("/farmer").post(async (req, res) => {
+
+    const { farmer_id, page } = req.body;
+    
+    try {
+        let size = await supabase.any(`SELECT count(*) FROM "FarmerLoan" WHERE "farmerId" = $1 AND "status" != $2`, [farmer_id, "ongoing"]);
+        let data = await supabase.any(`SELECT "id", "approvedAmount", "remainingAmount", "status", "approvalTime", "finishTime" FROM "FarmerLoan" WHERE "farmerId" = $1 AND "status" != $2 order by "id" limit 50 offset $3`, [farmer_id, "ongoing" ,page*50]);
+        const response = {
+            size: size[0].count,
+            data: data
+        }
+        res.json(response);
+    }
+    catch(err) {
+        console.log(err);
+        res.status(400).json("Error: Internal server error");
+    }
+});
+
+router.route("/sme").post(async (req, res) => {
+
+    const { sme_id, page } = req.body;
+    
+    try {
+        let size = await supabase.any(`SELECT count(*) FROM "SmeLoan" WHERE "smeId" = $1 AND "status" != $2`, [sme_id, "ongoing"]);
+        let data = await supabase.any(`SELECT "id", "approvedAmount", "remainingAmount", "status", "approvalTime", "finishTime" FROM "SmeLoan" WHERE "smeId" = $1 AND "status" != $2 order by "id" limit 50 offset $3`, [sme_id, "ongoing" ,page*50]);
+        const response = {
+            size: size[0].count,
+            data: data
+        }
+        res.json(response);
+    }
+    catch(err) {
+        console.log(err);
+        res.status(400).json("Error: Internal server error");
+    }
+});
+
+module.exports = router;
