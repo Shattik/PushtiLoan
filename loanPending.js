a613bb29b1238afea9f9a99eec5acb1d6aa2d0b4
tahmid-404-20 <1905002@ugrad.cse.buet.ac.bd>
2024-01-25 12:57:20 +0600

    updated loan_history queries

--- loanPending.js
+++ loanPending.js
@@ -2,37 +2,103 @@ const supabase = require("./db.js");
 const router = require("express").Router();
 
 router.route("/farmer").post(async (req, res) => {
-
-    try {
-        const {agent_id} = req.body;
-        let data = await supabase.any(`SELECT "id", "farmerId", "requestedMin", "requestedMax", "description", "status", "requestTime" FROM "FarmerLoan" WHERE "agentId" = $1 AND ("status" = $2 OR "status" = $3 OR "status" = $4)` , [agent_id, "pending", "inspected", "interviewed"]);
-        res.json(data); 
-    }
-    catch (err) {
-        console.log(err);
-        const response = {
-            error: "Error: Internal server error"
-        }
-        res.status(400).json(response);
-    }
-
+  try {
+    const { agent_id } = req.body;
+    console.log(agent_id);
+    let data = await supabase.any(
+      `select "request_id", "requestedMin", "requestedMax", "requestTime", "status", "rank", "points", "farmerType", "name" as farmerName, "avatarLink", "phone"\ 
+        from\
+        (select\
+          "request_id", "farmerId", "requestedMin", "requestedMax", "requestTime", "status", "rank", "points", "farmerType"\
+        from\
+          (\
+            select\
+              "id" as request_id,\
+              "agentId",\
+              "farmerId",\
+              "requestedMin",\
+              "requestedMax",\
+              "requestTime",\
+              "status"\
+            from\
+              "FarmerLoan"\
+            where\
+              "agentId" = $1 and ("status" = 'pending' or "status" = 'inspected' or "status" = 'interviewed')\
+              order by "requestTime" desc\
+          ) as A\
+          cross join (\
+            select\
+              "id",\
+              "rank",\
+              "points",\
+              "farmerType"\
+            from\
+              "Farmer"\
+          ) as B\
+        where\
+          b."id" = A."farmerId") as C\
+          cross join\
+          (select "id", "name", "avatarLink", "phone" from "User") as D\
+          where d."id" = c."farmerId";`,
+      [agent_id]
+    );
+    res.json(data);
+  } catch (err) {
+    console.log(err);
+    const response = {
+      error: "Error: Internal server error",
+    };
+    res.status(400).json(response);
+  }
 });
 
 router.route("/sme").post(async (req, res) => {
-
-    try {
-        const {agent_id} = req.body;
-        let data = await supabase.any(`SELECT "id", "smeId", "requestedMin", "requestedMax", "description", "status", "requestTime" FROM "SmeLoan" WHERE "agentId" = $1 AND ("status" = $2 OR "status" = $3 OR "status" = $4)` , [agent_id, "pending", "inspected", "interviewed"]);
-        res.json(data); 
-    }
-    catch (err) {
-        console.log(err);
-        const response = {
-            error: "Error: Internal server error"
-        }
-        res.status(400).json(response);
-    }
-    
+  try {
+    const { agent_id } = req.body;
+    let data = await supabase.any(
+      `select "request_id", "requestedMin", "requestedMax", "requestTime", "status", "rank", "points", "name" as smeName, "avatarLink", "phone"\ 
+      from\
+      (select\
+        "request_id", "smeId", "requestedMin", "requestedMax", "requestTime", "status", "rank", "points"\
+      from\
+        (\
+          select\
+            "id" as request_id,\
+            "agentId",\
+            "smeId",\
+            "requestedMin",\
+            "requestedMax",\
+            "requestTime",\
+            "status"\
+          from\
+            "SmeLoan"\
+          where\
+            "agentId" = $1 and ("status" = 'pending' or "status" = 'inspected' or "status" = 'interviewed')\
+            order by "requestTime" desc\
+        ) as A\
+        cross join (\
+          select\
+            "id",\
+            "rank",\
+            "points"\
+          from\
+            "Sme"\
+        ) as B\
+      where\
+        b."id" = A."smeId") as C\
+        cross join\
+        (select "id", "name", "avatarLink", "phone" from "User") as D\
+        where d."id" = c."smeId";`,
+      [agent_id]
+    );
+    res.json(data);
+  } catch (err) {
+    console.log(err);
+    const response = {
+      error: "Error: Internal server error",
+    };
+    res.status(400).json(response);
+  }
 });
 
 module.exports = router;
