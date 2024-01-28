d24d17ed2d12f50d465d730a4a78479dfcf4ea74
tahmid-404-20 <1905002@ugrad.cse.buet.ac.bd>
2024-01-28 07:33:59 +0600

    history - current loan condition updated

--- loanHistory.js
+++ loanHistory.js
@@ -4,11 +4,13 @@ const router = require("express").Router();
 router.route("/farmer").post(async (req, res) => {
 
     const { farmer_id, page } = req.body;
+
+    console.log(farmer_id, page);
     
     try {
         let size = await supabase.any(`SELECT count(*) FROM "FarmerLoan" WHERE "farmerId" = $1 AND "status" != $2`, [farmer_id, "ongoing"]);
-        let data = await supabase.any(`SELECT "id", "approvedAmount", "remainingAmount", "status", "approvalTime", "finishTime" FROM "FarmerLoan" WHERE "farmerId" = $1 AND "status" != $2 order by "id" limit 50 offset $3`, [farmer_id, "ongoing" ,page*50]);
-        let ongoing = await supabase.any(`SELECT "id", "approvedAmount", "remainingAmount", "status", "approvalTime", "finishTime" FROM "FarmerLoan" WHERE "farmerId" = $1 AND "status" = $2`, [farmer_id, "ongoing"]);
+        let data = await supabase.any(`SELECT "id", "approvedAmount", "remainingAmount", "status", "approvalTime", "finishTime" FROM "FarmerLoan" WHERE "farmerId" = $1 AND "status" != $2 order by "id" DESC limit 10 offset $3`, [farmer_id, "ongoing" ,page*10]);
+        let ongoing = await supabase.any(`SELECT "id", "requestedMin", "requestedMax", "approvedAmount", "remainingAmount", "status", "approvalTime", "requestTime", "approvalTime" FROM "FarmerLoan" WHERE "farmerId" = $1 AND ("status" = $2 OR "status" = $3 OR "status" = $4 OR "status" = $5)`, [farmer_id, "ongoing", "pending", "interviewed", "inspected"]);
         const response = {
             size: size[0].count,
             data: data
@@ -36,8 +38,8 @@ router.route("/sme").post(async (req, res) => {
     
     try {
         let size = await supabase.any(`SELECT count(*) FROM "SmeLoan" WHERE "smeId" = $1 AND "status" != $2`, [sme_id, "ongoing"]);
-        let data = await supabase.any(`SELECT "id", "approvedAmount", "remainingAmount", "status", "approvalTime", "finishTime" FROM "SmeLoan" WHERE "smeId" = $1 AND "status" != $2 order by "id" limit 50 offset $3`, [sme_id, "ongoing" ,page*50]);
-        let ongoing = await supabase.any(`SELECT "id", "approvedAmount", "remainingAmount", "status", "approvalTime", "finishTime" FROM "SmeLoan" WHERE "smeId" = $1 AND "status" = $2`, [sme_id, "ongoing"]);
+        let data = await supabase.any(`SELECT "id", "approvedAmount", "remainingAmount", "status", "approvalTime", "finishTime" FROM "SmeLoan" WHERE "smeId" = $1 AND "status" != $2 order by "id" DESC limit 10 offset $3`, [sme_id, "ongoing" ,page*10]);
+        let ongoing = await supabase.any(`SELECT "id", "requestedMin", "requestedMax", "approvedAmount", "remainingAmount", "status", "approvalTime", "requestTime", "approvalTime" FROM "SmeLoan" WHERE "smeId" = $1 AND ("status" = $2 OR "status" = $3 OR "status" = $4 OR "status" = $5)`, [sme_id, "ongoing", "pending", "interviewed", "inspected"]);
         const response = {
             size: size[0].count,
             data: data
